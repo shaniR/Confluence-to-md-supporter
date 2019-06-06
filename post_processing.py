27c1bb35fae7540f1566dc63e628201179e37b0b
shanira14 <shanira14@gmail.com>
2019-06-06 18:53:13 +0530

    add capability to remove unnecessary stars and improved code block formatting logic

--- post_processing.py
+++ post_processing.py
@@ -1,7 +1,13 @@
-import sys,os
+import sys, os
 import ntpath
+#import ptvsd
 
-EXTRA_SPACES = 4
+# 5678 is the default attach port in the VS Code debug configurations
+#print("Waiting for debugger attach")
+#ptvsd.enable_attach(address=('localhost', 5005), redirect_output=True)
+#ptvsd.wait_for_attach()
+#breakpoint()
+
 
 print '-------------------   Post processing started!  -------------------------'
 
@@ -10,63 +16,64 @@ md_files = os.listdir(md_source_path)
 md_dest_folder = sys.argv[2]
 
 for f in md_files:
-	
-	isFomattingOn = False
-	leadingSpaces = 0
 
-	toFormat = False
-	toFormatPrevious = False
+    inCodeBlock = False
+    insideMacro = False
+    leadingSpaces = 0
+    threeQuotes = '```'
 
-	terms = ["!!! tip", "!!! warning", "!!! info", "!!! note"]
+    terms = ["!!! tip", "!!! warning", "!!! info", "!!! note"]
 
-        fileName = ntpath.basename(f)
-	f = open(md_source_path+f, 'r')
-	lines = f.readlines()
-	f.close()
+    fileName = ntpath.basename(f)
+    f = open(md_source_path + f, 'r')
+    lines = f.readlines()
+    f.close()
 
-	for term in terms:
-	    for id, currentLine in enumerate(lines):
+    # Formatting content inside the terms above
+    for id, currentLine in enumerate(lines):
+        if(not insideMacro):
+            for term in terms:
+                if term in currentLine:
+                    insideMacro = True
+                    leadingSpaces = len(currentLine) - len(currentLine.lstrip())
+                    break
+        if (threeQuotes in currentLine):
+                #Identify the end of the code block.
+                if (not inCodeBlock):
+                    inCodeBlock = True
+                    leftSpaces = len(currentLine) - len(currentLine.lstrip())
+                    # making the start of the code block 4 spaces indented in relative to the macro
+                    if (leftSpaces <= leadingSpaces): 
+                        leadingSpaces = leftSpaces +4
+                    else:
+                        leadingSpaces = leftSpaces
+                else:
+                    inCodeBlock = False
+                    string_length=len(currentLine)+leftSpaces    # adding extra spaces
+                    currentLine=currentLine.lstrip().rjust(string_length)
+                    leftSpaces =0 
+                continue 
+        if (inCodeBlock or insideMacro):
+                string_length=len(currentLine)+leadingSpaces    # adding extra spaces
+                currentLine=currentLine.rjust(string_length)
+        # reset counters
+        if ("TEST" in currentLine):
+                leadingSpaces = 0
+                inCodeBlock =False
+                leftSpaces =0
+                insideMacro =False 
+        lines[id] = currentLine
 
-		if(terms.index(term) == 0):
-			if (currentLine.rstrip().lstrip() == "```"):
-				leadingSpaces = leadingSpaces - 4
-				toFormat = toFormatPrevious
-		                toFormat = True
-				if(toFormat):
-		    			leadingSpaces = 0
-	       
-		if term in currentLine:
-		    toFormat = True
-		    EXTRA_SPACES = 0
-		    leadingSpaces = len(currentLine) - len(currentLine.lstrip())
-		    if(leadingSpaces == 0):
-		        leadingSpaces = 4 
-		    continue
+fileNameLength = len(fileName)
+#Replace _ with - in the file name.
+fileName = fileName[1:fileNameLength].replace('_', '-').replace('-.md', '.md')
+fileName = fileName[0].lower() + fileName[1:]
 
-		if toFormat:
-		    currentLine = currentLine.rjust(leadingSpaces + EXTRA_SPACES + len(currentLine))
-		    if("TEST" in currentLine):
-		        toFormat = False
-		        EXTRA_SPACES = 0
-		        leadingSpaces = 0
+fileName = md_dest_folder + fileName
 
-		if(terms.index(term) == 0):
-			if "``` " in currentLine:
-	       			leadingSpaces = len(currentLine) - len(currentLine.lstrip())
-	       			toFormatPrevious = toFormat
+with open(fileName, "wb") as file:
+    for item in lines:
+        if ("TEST" not in item):
+            file.write("%s" % item)
 
-		lines[id] = currentLine
-
-
-        fileNameLength =len(fileName)
-	fileName = fileName[1:fileNameLength].replace('_', '-').replace('-.md', '.md') 
-	fileName = fileName[0].lower() + fileName[1:]
-
-        fileName = md_dest_folder+fileName
-
-	with open(fileName, "wb") as file:
-	    for item in lines:
-		if ("TEST" not in item):
-		    file.write("%s" % item)
-	
 print '-------------------   Post processing completed!  -------------------------'
--- pre_processing.py
+++ pre_processing.py
@@ -17,7 +17,7 @@ for f in files(html_source_folder):
 
 	soup = BeautifulSoup(open(html_source_folder+"/"+f), "html.parser")
 
-
+        #Remove span tags for note, tip, warning, info
 	for divInfo1 in soup.find_all("span", {'class':'aui-icon aui-icon-small aui-iconfont-approve confluence-information-macro-icon'}): 
 		divInfo1.unwrap()
 
@@ -30,35 +30,56 @@ for f in files(html_source_folder):
 	for divNote2 in soup.find_all("span", {'class':'aui-icon aui-icon-small aui-iconfont-warning confluence-information-macro-icon'}): 
 		divNote2.unwrap()
 
+        #Remove empty span tags
 	for emptySpan in soup.find_all("span", {'class':''}): 
 		emptySpan.unwrap()
 
+        #Extract all attachments  
 	for attachments in soup.find_all("div", {'class' :'pageSection group'}):
 		attachments.extract()
 
+        #Remove all confluence-information-macro-body class div tags
 	for divBody in soup.find_all("div", {'class':'confluence-information-macro-body'}):
 		divBody.unwrap()
 
+        # Mark the start of the note/tip/warning/info with !!! note/!!! tip/ !!! warning/ !!! info and the end with TEST
 	for divTip0 in soup.find_all("div", {'class':'confluence-information-macro-tip'}):
 		divTip0.insert_before('!!! tip')
+                #Add line break after the TEST line.
+                lineBreak = soup.new_tag('br')
+                divTip0.insert_after(lineBreak)
 		divTip0.insert_after("TEST")
 		divTip0.unwrap()
 
-	for divInfo in soup.find_all("div", {'class':' confluence-information-macro-information'}): 
+	for divInfo in soup.find_all("div", {'class':'confluence-information-macro-information'}): 
 		divInfo.insert_before('!!! info')
+                lineBreak = soup.new_tag('br')
+                #Add line break after the TEST line.
+                divInfo.insert_after(lineBreak)
 		divInfo.insert_after("TEST")
 		divInfo.unwrap()
 
 	for divWarning in soup.find_all("div", {'class':'confluence-information-macro-warning'}): 
 		divWarning.insert_before('!!! warning')
+                lineBreak = soup.new_tag('br')
+                #Add line break after the TEST line.
+                divWarning.insert_after(lineBreak)
 		divWarning.insert_after("TEST")
 		divWarning.unwrap()
 
 
 	for divNote in soup.find_all("div", {'class':'confluence-information-macro-note'}): 
 		divNote.insert_before('!!! note')
+                lineBreak = soup.new_tag('br')
+                #Add line break after the TEST line.
+                divNote.insert_after(lineBreak)
 		divNote.insert_after("TEST")
 		divNote.unwrap()
+
+        #Removing unnecessary stars appear due to tags such as <strong><br/></strong>
+        for emptyStrong in soup.find_all("strong"):
+                if (emptyStrong.next_element == soup.new_tag('br')):
+                    emptyStrong.decompose()
 
 	html =soup.contents
 	html = soup.prettify("utf-8")
